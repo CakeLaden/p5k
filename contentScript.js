e6e524d9e890916170993c004031c584a54b3f6b
CakeLaden <clayton.kyle.m@gmail.com>
2024-06-04 12:17:26 -0400

    Original Model A

--- contentScript.js
+++ contentScript.js
@@ -1,12 +1,22 @@
-function addReviewLinks() {
-  const albumGridItems = document.querySelectorAll('.summary-item');
+function addReviewScores() {
+  const albumGridItems = document.querySelectorAll('.review-grid__item');
   albumGridItems.forEach((item) => {
-    const albumLink = item.querySelector('.summary-item__image-link');
-    const artistName = item.querySelector('.summary-item__sub-hed');
-    const reviewLinkParagraph = document.createElement('p');
-    reviewLinkParagraph.textContent = albumLink.href;
-    artistName.parentNode.insertBefore(reviewLinkParagraph, artistName.nextSibling);
+    const albumLink = item.querySelector('.review-grid__item__link');
+    const artistName = item.querySelector('.review-grid__item__artist');
+    fetch(albumLink.href)
+      .then((response) => response.text())
+      .then((html) => {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, 'text/html');
+        const scoreCircle = doc.querySelector('div[class^="ScoreCircle-"]');
+        const rating = scoreCircle.querySelector('p[class^="Rating-"]');
+        const reviewScore = rating.textContent;
+        const reviewScoreParagraph = document.createElement('p');
+        reviewScoreParagraph.textContent = reviewScore;
+        artistName.parentNode.insertBefore(reviewScoreParagraph, artistName.nextSibling);
+      })
+      .catch((error) => console.error(error));
   });
 }
 
-addReviewLinks();
+addReviewScores();
